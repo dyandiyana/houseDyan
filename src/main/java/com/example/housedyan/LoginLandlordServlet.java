151108b267a99a17660952fb46117b1836d6df8b
Diyanah Jafridin <2021118575@student.uitm.edu.my>
2022-01-23 21:35:05 +0800

    upload file

--- src/main/java/com/example/housedyan/LoginLandlordServlet.java
+++ src/main/java/com/example/housedyan/LoginLandlordServlet.java
@@ -47,7 +47,7 @@ public class LoginLandlordServlet extends HttpServlet {
 
                 while (res.next()){
 
-                    if(lUsername.equals(lUsername)&&lPassword.equals(lPassword)) {
+                    if(lUsername.equals(sql)&&lPassword.equals(sql)) {
                         //out.println("<p>" + "Email: "+ res.getString("landlordemail") + "</p>");
                         response.sendRedirect("tenant-homepage.jsp");
                     }
