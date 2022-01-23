f9af5368e0eaf021ba6c7b3f82eebe6ee5f6b75c
Anis Zamri <2021118375@student.uitm.edu.my>
2022-01-23 20:57:56 +0800

    tryy commit

--- src/main/java/com/example/housedyan/ReadLandlordProfileServlet.java
+++ src/main/java/com/example/housedyan/ReadLandlordProfileServlet.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @WebServlet(name = "ReadLandlordProfileServlet", value = "/ReadLandlordProfileServlet")
 public class ReadLandlordProfileServlet extends HttpServlet {
@@ -15,26 +16,62 @@ public class ReadLandlordProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
 
-        HttpSession session = request.getSession(); // if nk wujudkan session mcm sir buat
-
-        // tarik value dri form dalam jsp lalu kot javascript bawah tu
-        String lId = (String) session.getAttribute("landlordId");
-        String lUsername = (String) session.getAttribute("landlordUsername");
-        String lPassword = (String) session.getAttribute("landlordPassword");
-        String lName = (String) session.getAttribute("landlordName");
-        String lEmail = (String) session.getAttribute("landlordEmail");
-        String lAge = (String) session.getAttribute("landlordAge");
-        String lPhoneNo = (String) session.getAttribute("landordPhoneNo");
-        String lGender = (String) session.getAttribute("landlordGender");
-
-        //masukkan value ke dlm java class StudentDetails
-        CreateAccount readAccount= new CreateAccount(lId,lUsername,lPassword,lName,lEmail,lAge,lPhoneNo,lGender);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
+        try{
+            //  dlm parameter ni pastikan nama sama dalam form jsp name=""
+
+            String lId = request.getParameter("landlordId");
+            String lUsername = request.getParameter("landlordUsername");
+            String lPassword = request.getParameter("landlordPassword");
+            String lName = request.getParameter("landlordName");
+            String lEmail = request.getParameter("landlordEmail");
+            String lAge = request.getParameter("landlordAge");
+            String lPhoneNo = request.getParameter("landlordPhoneNo");
+            String lGender = request.getParameter("landlordGender");
+
+            //nk testing keluar masuk data pastikan xampp, heroku , database connected
+
+            Class.forName("org.postgresql.Driver"); // ni stay
+            String dbURL = "jdbc:postgresql://ec2-34-194-171-47.compute-1.amazonaws.com:5432/d6u31lk8tofpbt"; //ni url dri heroku database
+            String user = "nhydysucefvvzn"; //ni user dri heroku database
+            String pass = "d91c6a95779ab44ec2939ae0225389a20d7129541e10791714dbf4f165e0d078"; //ni password dri heroku database
+            String query = "Select landlordid,landlordusername,landlordpassword,landlordname,landlordemail,landlordage,landlordphoneno, landlordgender from landlord";
+
+            StudentDetails studentDetails= null;
+            Connection conn = DriverManager.getConnection(dbURL, user, pass);
+            // klau buat postgress atas2 ni amik yg details dri heroku
+
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1,sdID1);
+            System.out.println(preparedStatement);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next())
+            {
+                System.out.print("Landlord Id: " + rs.getString("landlordid"));
+                System.out.print("Username: " + rs.getString("landlordusername"));
+                System.out.print("landlord password :" + rs.getString("landlordpassword"));
+                System.out.print("landlord name: " + rs.getString("landlordname"));
+                System.out.print("landlord email: " + rs.getString("landlordemail"));
+                System.out.print("landlord age: " + rs.getString("landlordage"));
+                System.out.print("landlord phone: " + rs.getString("landlordphoneno"));
+                System.out.print("landlord gender: " + rs.getString("landlordgender"));
+
+            }
+
+        }catch(Exception e){
+            out.println(e);
+        }
+
+
+    }
     }
 }
 
