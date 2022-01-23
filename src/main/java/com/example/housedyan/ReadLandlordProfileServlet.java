de3d3d82dd73607e5bde3888df02fbb04ddc4f94
Anis Zamri <2021118375@student.uitm.edu.my>
2022-01-23 20:32:41 +0800

    tryy commit

---
+++ src/main/java/com/example/housedyan/ReadLandlordProfileServlet.java
@@ -0,0 +1,19 @@
+package com.example.housedyan;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ReadLandlordProfileServlet", value = "/ReadLandlordProfileServlet")
+public class ReadLandlordProfileServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
