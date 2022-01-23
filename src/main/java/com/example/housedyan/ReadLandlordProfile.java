d17114d1cc0cbb8a8193a1818961a2719211d268
Anis Zamri <2021118375@student.uitm.edu.my>
2022-01-23 20:29:26 +0800

    tryy commit

--- src/main/java/com/example/housedyan/ReadLandlordProfile.java
+++ src/main/java/com/example/housedyan/ReadLandlordProfile.java
@@ -15,5 +15,81 @@ public class ReadLandlordProfile implements java.io.Serializable
     {
 
     }
+
+    public ReadLandlordProfile(String landordid,String landlordusername,String landlordpassword,String landlordname,String landlordemail,String landlordage,String landlordphoneno,String landlordgender)
+    {
+        this.landordid=landordid;
+        this.landlordusername=landlordusername;
+        this.landlordpassword=landlordpassword;
+        this.landlordname=landlordname;
+        this.landlordemail=landlordemail;
+        this.landlordage=landlordage;
+        this.landlordphoneno=landlordphoneno;
+        this.landlordgender=landlordgender;
+    }
+
+    public String getLandordid() {
+        return landordid;
+    }
+
+    public String getLandlordusername() {
+        return landlordusername;
+    }
+
+    public String getLandlordpassword() {
+        return landlordpassword;
+    }
+
+    public String getLandlordname() {
+        return landlordname;
+    }
+
+    public String getLandlordemail() {
+        return landlordemail;
+    }
+
+    public String getLandlordage() {
+        return landlordage;
+    }
+
+    public String getLandlordphoneno() {
+        return landlordphoneno;
+    }
+
+    public String getLandlordgender() {
+        return landlordgender;
+    }
+
+    public void setLandordid(String landordid) {
+        this.landordid = landordid;
+    }
+
+    public void setLandlordusername(String landlordusername) {
+        this.landlordusername = landlordusername;
+    }
+
+    public void setLandlordpassword(String landlordpassword) {
+        this.landlordpassword = landlordpassword;
+    }
+
+    public void setLandlordname(String landlordname) {
+        this.landlordname = landlordname;
+    }
+
+    public void setLandlordemail(String landlordemail) {
+        this.landlordemail = landlordemail;
+    }
+
+    public void setLandlordage(String landlordage) {
+        this.landlordage = landlordage;
+    }
+
+    public void setLandlordphoneno(String landlordphoneno) {
+        this.landlordphoneno = landlordphoneno;
+    }
+
+    public void setLandlordgender(String landlordgender) {
+        this.landlordgender = landlordgender;
+    }
 }
 
