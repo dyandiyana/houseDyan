b972d6f40bfa96cc35bd913b6811f3a86de543f8
Diyanah Jafridin <2021118575@student.uitm.edu.my>
2022-01-23 20:53:09 +0800

    upload file

---
+++ src/main/java/com/example/housedyan/LoginLandlordServlet.java
@@ -0,0 +1,65 @@
+package com.example.housedyan;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "LoginLandlordServlet", value = "/LoginLandlordServlet")
+public class LoginLandlordServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        try{
+            //  dlm parameter ni pastikan nama sama dalam form jsp name=""
+
+
+            String lUsername = request.getParameter("landlordUsername");
+            String lPassword = request.getParameter("landlordPassword");
+
+            if(lUsername.equals(lUsername)&&lPassword.equals(lPassword))
+            {
+                response.sendRedirect("tenant-homepage.jsp");
+            }
+            Class.forName("org.postgresql.Driver"); // ni stay
+            String dbURL = "jdbc:postgresql://ec2-34-194-171-47.compute-1.amazonaws.com:5432/d6u31lk8tofpbt"; //ni url dri heroku database
+            String user = "nhydysucefvvzn"; //ni user dri heroku database
+            String pass = "d91c6a95779ab44ec2939ae0225389a20d7129541e10791714dbf4f165e0d078"; //ni password dri heroku database
+            Connection conn = DriverManager.getConnection(dbURL, user, pass);
+
+            String sql  ="SELECT landlordusername,landlordpassword,landlordemail from landlord";
+
+            if (conn != null){
+                DatabaseMetaData dm = conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product Name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+
+                Statement statement = conn.createStatement();
+                ResultSet res = statement.executeQuery(sql);
+
+                while (res.next()){
+
+                    if(lUsername.equals(lUsername)&&lPassword.equals(lPassword)) {
+                        out.println("<p>" + "Email: "+ res.getString("landlordemail") + "</p>");
+                    }
+                }
+            }
+
+        }catch(Exception e){
+            out.println(e);
+        }
+
+    }
+}
