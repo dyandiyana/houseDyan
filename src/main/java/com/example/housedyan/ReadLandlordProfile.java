de4530d3ea810e3d670d822ab1df73f3cc837c28
Anis Zamri <2021118375@student.uitm.edu.my>
2022-01-23 20:24:07 +0800

    tryy3 commit

---
+++ src/main/java/com/example/housedyan/ReadLandlordProfile.java
@@ -0,0 +1,19 @@
+package com.example.housedyan;
+
+public class ReadLandlordProfile implements java.io.Serializable
+{
+    public String landordid;
+    public String landlordusername;
+    public String landlordpassword;
+    public String landlordname;
+    public String landlordemail;
+    public String landlordage;
+    public String landlordphoneno;
+    public String landlordgender;
+
+    public ReadLandlordProfile()
+    {
+
+    }
+}
+
