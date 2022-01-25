84d330f708ff46b89ea15016fc137be3f049111d
Anis Zamri <2021118375@student.uitm.edu.my>
2022-01-25 13:38:59 +0800

    tryy commit

--- src/main/java/com/example/housedyan/DeleteLandlordServlet.java
+++ src/main/java/com/example/housedyan/DeleteLandlordServlet.java
@@ -8,40 +8,45 @@ import java.io.PrintWriter;
 import java.sql.*;
 
 @WebServlet(name = "DeleteLandlordServlet", value = "/DeleteLandlordServlet")
-public class DeleteLandlordServlet extends HttpServlet {
+public class DeleteLandlordServlet extends HttpServlet
+{
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+
+
+        //int hids = Integer.parseInt(request.getParameter("hid"));
+        int landlordid = Integer.parseInt(request.getParameter("landlordid"));
+
         try
         {
-            int landlordusername = Integer.parseInt(request.getParameter("landlordusername"));
-            System.out.println(landlordusername);
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
 
             Class.forName("org.postgresql.Driver"); // ni stay
             String dbURL = "jdbc:postgresql://ec2-34-194-171-47.compute-1.amazonaws.com:5432/d6u31lk8tofpbt"; //ni url dri heroku database
-            String user = "nhydysucefvvzn";
-            String pass = "d91c6a95779ab44ec2939ae0225389a20d7129541e10791714dbf4f165e0d078";
+            String user = "nhydysucefvvzn"; //ni user dri heroku database
+            String pass = "d91c6a95779ab44ec2939ae0225389a20d7129541e10791714dbf4f165e0d078"; //ni password dri heroku database
             Connection conn = DriverManager.getConnection(dbURL, user, pass);
 
-            Statement stmt = conn.createStatement();
-            PreparedStatement pst = conn.prepareStatement("delete * from landlord");
-            //pst.setInt(1,landlordid);
-            ResultSet rs= pst.executeQuery();
+            PreparedStatement pst = conn.prepareStatement("DELETE FROM landlord WHERE landlordid = ?");
+            //pst.setInt(1, hids);
+            pst.setInt(1,landlordid);
 
-            out.print("Your Requested Data Is Deleted");
-            conn.close();
+            pst.executeUpdate();
+
+
         }
 
         catch (Exception e)
         {
-            System.out.println("Error: " + e.toString());
-
+            System.out.println(e);
         }
+
+        response.sendRedirect("landlord-viewProfile.jsp");
     }
 }
